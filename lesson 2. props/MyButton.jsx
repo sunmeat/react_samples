6b6edfed39fd582e1fba88a1b2258acdb52ed757
Oleksandr Zahoruiko <sunmeatrich@gmail.com>
2025-08-26 13:29:06 +0300

    Update MyButton.jsx

--- lesson 2. props/MyButton.jsx
+++ lesson 2. props/MyButton.jsx
@@ -3,7 +3,7 @@ import './MyButton.css';
 export function MyButton(props) {
     return (
         <button className="my-button" onClick={props.onClick}>
-            {props.label} (Кликнули {props.clickCount} раз)
+            {props.label} (Клацнули {props.clickCount} разів)
         </button>
     );
 }
