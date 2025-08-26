63202f01713358926706b7079a8adf159932f3e1
Oleksandr Zahoruiko <sunmeatrich@gmail.com>
2025-08-26 13:28:38 +0300

    Update Header.jsx

--- lesson 2. props/Header.jsx
+++ lesson 2. props/Header.jsx
@@ -4,7 +4,8 @@ export function Header({ title, userCount, theme }) {
     return (
         <header className="header" style={{ backgroundColor: theme.background, color: theme.color }}>
             <h1 className="header-title">{title}</h1>
-            <p style={{ textAlign: theme.align }}>Активных пользователей по всему миру прямо сейчас: {userCount}</p>
+            <p style={{ textAlign: theme.align }}>Активних користувачів по усьому світі прям зараз: {userCount}</p>
         </header>
     );
+
 }
