d44105194a87b573009515f719ffee526fb005d4
Oleksandr Zahoruiko <sunmeatrich@gmail.com>
2025-08-26 13:38:43 +0300

    Update Content.jsx

--- lesson 2. events/Content.jsx
+++ lesson 2. events/Content.jsx
@@ -7,33 +7,33 @@ export function Content({welcomeMessage, settings, ButtonComponent}) {
     };
 
     const handleInputFocus = () => {
-        console.log('Поле в фокусе');
+        console.log('Поле у фокусі');
     };
 
     const handleInputBlur = () => {
-        console.log('Поле потеряло фокус');
+        console.log('Поле втратило фокус');
     };
 
     return (
         <main className="content">
             <h2 className="content-title">{welcomeMessage}</h2>
             <p>
-                Уровень сложности: {settings.difficulty} | Игроков из группы ПВ421: {settings.players}
+                Рівень складності: {settings.difficulty} | Гравців із групи СПР411: {settings.players}
             </p>
             <ButtonComponent/>
             <div>
                 <input
                     type="text"
                     className="content-input"
-                    placeholder="Введите текст..."
+                    placeholder="Введіть текст..."
                     onChange={handleInputChange}
                     onFocus={handleInputFocus}
                     onBlur={handleInputBlur}
-                    onKeyDown={(e) => console.log('Нажата клавиша:', e.key)}
-                    onKeyUp={(e) => console.log('Отпущена клавиша:', e.key)}
-                    onCopy={() => console.log('Текст скопирован')}
-                    onCut={() => console.log('Текст вырезан')}
-                    onPaste={() => console.log('Текст вставлен')}
+                    onKeyDown={(e) => console.log('Натиснуто клавішу:', e.key)}
+                    onKeyUp={(e) => console.log('Відпустили клавішу:', e.key)}
+                    onCopy={() => console.log('Текст скопійовано')}
+                    onCut={() => console.log('Текст вирізали')}
+                    onPaste={() => console.log('Текст вставили')}
                 />
             </div>
         </main>
